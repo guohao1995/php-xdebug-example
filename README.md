5acc59765facb4584887f7cb41fc62d679e79e10
Christopher Maldonado <chmald@microsoft.com>
2020-06-09 10:59:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Laravel Slow Example
+
+This is an example to use with XDEBUG to show slow response in PHP.
+
+## Setting up application
+
+### Install PHP dependancies
+
+```bash
+composer install
+```
+
+### Generate your APP key
+
+```bash
+php artisan key:generate
+```
+
+### .env file
+
+You will need a database to connect to your application.
+
+Copy the _.env.example_ file to _.env_
+
+```bash
+cp .env.example .env
+```
+
+Insert your database info inside your .env file
+
+```php
+DB_CONNECTION=mysql
+DB_HOST=mydbhost.mysql.database.azure.com
+DB_PORT=3306
+DB_DATABASE=mydbname
+DB_USERNAME=mydbuser
+DB_PASSWORD=mydbpass
+```
+
+### Seed Database
+
+```php
+php artisan migrate
+```
---
+++ app/Http/Controllers/SlowResponse.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SlowResponse extends Controller
+{
+    public function index() {
+        // We will simulate a slow response with Sleep();
+        sleep(15);
+        return 'Slow response';
+    }
+
+    public function slowNested() {
+        $this->goIntoSlowFunction();
+        return 'Another slow response';
+    }
+
+    private function goIntoSlowFunction(){
+        $count = 0;
+        $break = 100;
+        while($count < $break)
+        {
+            DB::table('tasks')->insert(
+                ['name' => 'Blarg']
+            );
+            $count++;
+        }
+        $tasks = DB::table('tasks')->get();
+
+        foreach($tasks as $task) {
+            $name = $task->name;
+        }
+    }
+}
